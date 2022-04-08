fb07215f4b1fec77ecbf80b56c67299c74a531d4
Frank Yang <yyf20001230@gmail.com>
2022-04-08 18:31:04 -0500

    price list

--- app.py
+++ app.py
@@ -7,26 +7,6 @@ from lstm import CryptoCurrencyPricePredictor
 
 app = Flask(__name__)
 
-day = 10
-model = CryptoCurrencyPricePredictor()
-model.build_and_train_model()
-price_predictions = model.predict(day)
-
-fig = Figure(figsize = (13, 7))
-ax = fig.subplots()
-ax.plot(model.hist['close'][:-day], label = "train", linewidth = 2)
-ax.plot(model.hist['close'][-day:], label = "test", linewidth = 2)
-ax.set_xlabel('time[year, month]', fontsize=10)
-ax.set_ylabel('price [USD]', fontsize=10)
-ax.legend(loc='best', fontsize=10)
-# Save it to a temporary buffer.
-
-buf = io.BytesIO()
-fig.savefig(buf, format="png")
-# Embed the result in the html output.
-data = base64.b64encode(buf.getbuffer()).decode("ascii")
-datalink = f"data:image/png;base64,{data}"
-
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
@@ -34,6 +14,7 @@ def home():
         model = CryptoCurrencyPricePredictor()
         model.build_and_train_model()
         price_predictions = model.predict(day)
+        print(price_predictions)
 
         fig = Figure(figsize = (13, 7))
         ax = fig.subplots()
@@ -55,6 +36,7 @@ def home():
         model = CryptoCurrencyPricePredictor()
         model.build_and_train_model()
         price_predictions = model.predict(day)
+        print(price_predictions)
 
         fig = Figure(figsize = (13, 7))
         ax = fig.subplots()
@@ -71,4 +53,4 @@ def home():
         data = base64.b64encode(buf.getbuffer()).decode("ascii")
         datalink = f"data:image/png;base64,{data}"      
 
-    return render_template("index.html", graph_img_link = datalink)
+    return render_template("index.html", graph_img_link = datalink, price_predictions = price_predictions)
