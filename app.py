f5c46cfb685cb5f6d3a04de3447207694dd59019
Frank Yang <yyf20001230@gmail.com>
2022-04-07 21:17:23 -0500

    bug fix with web

--- app.py
+++ app.py
@@ -49,5 +49,26 @@ def home():
         # Embed the result in the html output.
         data = base64.b64encode(buf.getbuffer()).decode("ascii")
         datalink = f"data:image/png;base64,{data}"
+    
+    else:
+        day = 10
+        model = CryptoCurrencyPricePredictor()
+        model.build_and_train_model()
+        price_predictions = model.predict(day)
+
+        fig = Figure(figsize = (13, 7))
+        ax = fig.subplots()
+        ax.plot(model.hist['close'][:-day], label = "train", linewidth = 2)
+        ax.plot(model.hist['close'][-day:], label = "test", linewidth = 2)
+        ax.set_xlabel('time[year, month]', fontsize=10)
+        ax.set_ylabel('price [USD]', fontsize=10)
+        ax.legend(loc='best', fontsize=10)
+        # Save it to a temporary buffer.
+
+        buf = io.BytesIO()
+        fig.savefig(buf, format="png")
+        # Embed the result in the html output.
+        data = base64.b64encode(buf.getbuffer()).decode("ascii")
+        datalink = f"data:image/png;base64,{data}"      
 
     return render_template("index.html", graph_img_link = datalink)
